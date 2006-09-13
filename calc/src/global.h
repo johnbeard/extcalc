a397bc8f7b9173a20519f923127d222a64bebf94
rainer1223 <rainer1223@0696df40-9f1b-0410-b5a6-9e504e5fbbef>
2006-09-13 12:05:43 +0000

    Fixed bug with random number generation on Solaris

--- calc/src/global.h
+++ calc/src/global.h
@@ -14,7 +14,7 @@
 #include "list.h"
 
 
-#define VERSIONSTRING "Version: 0.5.0 2006-09-03\nCalculator algorithm: extcalc v0.5.0 2006-08-20"
+#define VERSIONSTRING "Version: 0.5.0 2006-09-11\nCalculator algorithm: extcalc v0.5.0 2006-08-20"
 #define AUTHORSTRING "Author:\nRainer Strobel\n2006\n"
 
 
